e2f77707db19770402f228ad6260bf54cefdeea0
zaki-coder <zakiazizi1841992@gmail.com>
2022-04-07 09:58:24 +0200

    node_modules ignore

--- db/conncectDB.js
+++ db/conncectDB.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 import {} from "dotenv/config";
 
-
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('connected to database');
+    console.log("connected to database");
   } catch (err) {
     console.log(err);
   }
 }
-
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = process.env.PORT || 4000;
 import { connectDB } from "./db/conncectDB.js";
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 connectDB();
-app.listen(port, () => console.log('The server is listening on port', port));
+app.listen(port, () => console.log("The server is listening on port", port));
