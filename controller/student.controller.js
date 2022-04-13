8b10404cfec6c5cce8cffcad90a029b45f5d1b12
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-04-13 16:07:47 +0200

    populate bei student fertig

--- controller/student.controller.js
+++ controller/student.controller.js
@@ -1,8 +1,12 @@
 const Student = require("../models/student.model");
+require("../models/teacher.model.js");
+require("../models/class.model");
+require("../models/homework.model")
+
 
 async function getStudents(req, res) {
   try {
-    const students = await Student.find({});
+    const students = await (await Student.find({}).populate("classId").populate("teachers").populate("homeworks"));
     res.status(200).json({ students, totalStudents: students.length });
   } catch (err) {
     res.status(500).json({ err });
@@ -25,19 +29,23 @@ async function createStudent(req, res) {
 }
 
 async function updateStudent(req, res) {
-  const student = await Student.updateOne(
+  try{ 
+  const student = await Student.findOneAndUpdate(
     { _id: req.params.id },
     req.body,
-    { new: true },
-    function (err) {
-      if (err) {
-        return res.status(500).json({ err });
-      }
-      return res
-        .status(200)
-        .json({ success, msg: "student successfully updated" });
+    { new: true,
+      runValidators: true,
+      context: "query", });
+      res.status(200).json({
+        message: "success",
+        data: student,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Fehler bei Teacher Update!",
+      });
+      console.error(error);
     }
-  );
 }
 
 async function deleteStudent(req, res) {
