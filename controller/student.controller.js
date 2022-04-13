f3bbe2bd8076092446d10c275ccca763259f7f9d
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-04-13 16:08:43 +0200

    await bei student gelöscht

--- controller/student.controller.js
+++ controller/student.controller.js
@@ -1,20 +1,22 @@
 const Student = require("../models/student.model");
 require("../models/teacher.model.js");
 require("../models/class.model");
-require("../models/homework.model")
-
+require("../models/homework.model");
 
 async function getStudents(req, res) {
   try {
-    const students = await (await Student.find({}).populate("classId").populate("teachers").populate("homeworks"));
+    const students =  await Student.find({})
+      .populate("classId")
+      .populate("teachers")
+      .populate("homeworks");
     res.status(200).json({ students, totalStudents: students.length });
   } catch (err) {
     res.status(500).json({ err });
   }
 }
 async function getStudent(req, res) {
-  const student = await Student.findOne({_id: req.params.id});
-  res.status(200).json({ student, msg: 'Gefunden' });
+  const student = await Student.findOne({ _id: req.params.id });
+  res.status(200).json({ student, msg: "Gefunden" });
 }
 
 async function createStudent(req, res) {
@@ -29,35 +31,33 @@ async function createStudent(req, res) {
 }
 
 async function updateStudent(req, res) {
-  try{ 
-  const student = await Student.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { new: true,
-      runValidators: true,
-      context: "query", });
-      res.status(200).json({
-        message: "success",
-        data: student,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: "Fehler bei Teacher Update!",
-      });
-      console.error(error);
-    }
+  try {
+    const student = await Student.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true, runValidators: true, context: "query" }
+    );
+    res.status(200).json({
+      message: "success",
+      data: student,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Fehler bei Teacher Update!",
+    });
+    console.error(error);
+  }
 }
 
 async function deleteStudent(req, res) {
-    const student = await Student.findById({_id: req.params.id});
-    if (student) {
-      await student.remove();
-      res.json({ message: "Student removed" });
-    } else {
-      res.status(404);
-      throw new Error("student not found");
-    }
-
+  const student = await Student.findById({ _id: req.params.id });
+  if (student) {
+    await student.remove();
+    res.json({ message: "Student removed" });
+  } else {
+    res.status(404);
+    throw new Error("student not found");
+  }
 }
 
 module.exports = {
@@ -65,5 +65,5 @@ module.exports = {
   getStudent,
   createStudent,
   updateStudent,
-  deleteStudent
+  deleteStudent,
 };
