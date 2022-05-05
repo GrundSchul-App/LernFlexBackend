13ad4c0e46659f7173ac5c62b04cf6203a2ca590
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-05-05 16:02:40 +0200

    homework controller

--- routes/homework.routes.js
+++ routes/homework.routes.js
@@ -9,23 +9,33 @@ const {
   getAllHomeworksBySubjectId,
   getAllHomeworksByType,
   getAllHomeworksByDate,
+  getAllHomeworksByTeacher,
 } = require("../controller/homework.controller.js");
 
 const homeworkRouter = new Router();
 
-homeworkRouter.route("/:teacherId").get(getAllHomeworks);
+// homeworkRouter.route("/:teacherId").get(getAllHomeworks);
+homeworkRouter.route("/").get(getAllHomeworks);
+
+homeworkRouter.route("/teacher/:teacherId").get(getAllHomeworksByTeacher);
 
 homeworkRouter.route("/:teacherId/title/:title").get(getAllHomeworksByTitle);
 
-homeworkRouter
+
+/* homeworkRouter
   .route("/:teacherId/subject/:subjectId")
+  .get(getAllHomeworksBySubjectId); */
+  homeworkRouter
+  .route("/subject/:subjectId")
   .get(getAllHomeworksBySubjectId);
 
-homeworkRouter.route("/:teacherId/type/:type").get(getAllHomeworksByType);
+// homeworkRouter.route("/:teacherId/type/:type").get(getAllHomeworksByType);
+homeworkRouter.route("/type/:type").get(getAllHomeworksByType);
 
 homeworkRouter.route("/:teacherId/date/:date").get(getAllHomeworksByDate);
 
-homeworkRouter.route("/:teacherId/add").post(addHomework);
+//homeworkRouter.route("/:teacherId/add").post(addHomework);
+homeworkRouter.route("/").post(addHomework);
 
 homeworkRouter
   .route("/:id")
